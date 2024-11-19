2ca6a33f33325c6ac654d0d73e27f309fbca0082
Media Shd. <mediashd@gmail.com>
2024-11-19 10:21:01 +0300

    Create sketchboard_lang.php
    Turkish language translation

---
+++ modules/sketchboard/language/turkish/sketchboard_lang.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['sketchboard']  = 'Çizim';
+$lang['sketch_board']  = 'Sketch Board';
+$lang['create_new_sketch_board']  = 'Yeni çizim tahtası oluştur';
+$lang['title']  = 'Başlık';
+$lang['description']  = 'Açıklama';
+$lang['sketch_board_details'] = 'Çizim Tahtası Detayları';
+$lang['shared_to'] = 'ile Paylaş';
+$lang['shared_with'] = 'ile Paylaşıldı';
+$lang['new_sketchboard_shared_with_you'] = '%s başlıklı çizim tahtası sizinle paylaşıldı';
