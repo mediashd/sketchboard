c9f2775414a4cb5a6357c10e479e92b59bfd325e
Media Shd. <mediashd@gmail.com>
2024-11-19 10:24:05 +0300

    Update sketchboard_lang.php

--- modules/sketchboard/language/turkish/sketchboard_lang.php
+++ modules/sketchboard/language/turkish/sketchboard_lang.php
@@ -2,8 +2,8 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 $lang['sketchboard']  = 'Çizim';
-$lang['sketch_board']  = 'Sketch Board';
-$lang['create_new_sketch_board']  = 'Yeni çizim tahtası oluştur';
+$lang['sketch_board']  = 'Çizim Tahtası';
+$lang['create_new_sketch_board']  = 'Yeni Çizim Tahtası Oluştur';
 $lang['title']  = 'Başlık';
 $lang['description']  = 'Açıklama';
 $lang['sketch_board_details'] = 'Çizim Tahtası Detayları';
